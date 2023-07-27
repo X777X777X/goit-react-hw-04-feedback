c6fa48b279447d2bc84879953a963df6d7884aa3
X777X777X <119849170+X777X777X@users.noreply.github.com>
2023-07-27 14:06:53 +0300

    Add files

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css'
+
+function FeedbackOptions({ options, onLeaveFeedback }) {
+    return options.map(option => (
+      <button
+        type="button"
+        className={`${css.btn} ${css[option.toLowerCase()]}`}
+        key={option}
+        onClick={() => onLeaveFeedback(option)}
+      >
+        {option}
+      </button>
+    ));
+}
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import css from './Notification.module.css'
+
+function Notification({message}) {
+    return <p className={css.message}>
+{message}
+    </p>
+}
+
+Notification.propTypes = {
+    message: PropTypes.string.isRequired,
+};
+
+export default Notification;
